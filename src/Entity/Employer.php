dcdd2431ded1538e8d7cbf68c743370b931b6bc6
Sadikh-SA <abougueye96@yahoo.fr>
2019-07-15 14:24:52 +0000

    Validation

--- src/Entity/Employer.php
+++ src/Entity/Employer.php
@@ -7,6 +7,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\EmployerRepository")
+ * 
  */
 class Employer
 {
@@ -18,40 +19,51 @@ class Employer
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     * @Assert\Length(min=3)
+     * @ORM\Column(name="matricule", type="string", length=255)
+     * @Assert\NotBlank(message="Ce champs doit être renseigné")
      */
     private $matricule;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="ce champs ne peut pas être vide")
      * @Assert\Length(min=8)
      */
     private $nomcomplet;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      * @Assert\Length(min=8)
      */
     private $adresse;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\NotBlank(message="ce champs ne peut pas être vide")
+     * @Assert\GreaterThan(10000, message="Un salaire inférieur à 10.000 FCFA? Sérieux vous avez pas de coeur vous.")
+     * @Assert\Positive
      */
     private $Salaire;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Service", inversedBy="employers")
+     * @Assert\NotBlank(message="ce champs ne peut pas être vide")
+     * 
      */
     private $idService;
 
     /**
      * @ORM\Column(type="date")
+     * @Assert\Date()
+     * @Assert\LessThan("-18 years", message="tu veux faire travailler des enfants? Tu mérite la mort")
+     * @Assert\NotBlank(message="ce champs ne peut pas être vide")
      */
     private $ddn;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\NotBlank(message="Ce champs ne peut pas être vide")
      */
     private $cni;
 
